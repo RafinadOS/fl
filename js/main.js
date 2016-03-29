3cbd39017993395c33283030969eb634c05e2f1a
Павел Рафеев < >
2016-03-29 18:23:28 +0200

    fix

--- js/main.js
+++ js/main.js
@@ -10,9 +10,6 @@ $(function(){
 
     $('label').click(function(){
 
-    	$('.total-2').hide(0);
-    	$('.total').addClass('total-show')
-
     	var atr 	= $(this).attr('for');
     	var chk1 	= $('#chk-left-1').attr('checked');
     	var chk2 	= $('#chk-right-1').attr('checked');
@@ -22,20 +19,15 @@ $(function(){
     		var chk13 	= $('#chk-left-3').attr('checked');
 			if (chk12 == 'checked' && chk13 == 'checked') {
 	    		$('#chk-left-1').attr('checked','checked');
-	    		$('.bottom__small').text('программа');
-	    		$('.bottom__big').text('сжигание жира');
 	    	}
 	    	else {
 	    		$('#chk-left-1').removeAttr('checked');
 	    	}
 
-
 	    	var chk22 	= $('#chk-right-2').attr('checked');
     		var chk23 	= $('#chk-right-3').attr('checked');
 			if (chk22 == 'checked' && chk23 == 'checked') {
 	    		$('#chk-right-1').attr('checked','checked');
-	    		$('.bottom__small').text('шаг 2');
-	    		$('.bottom__big').text('моделирование фигуры');
 	    	}
 	    	else {
 	    		$('#chk-right-1').removeAttr('checked');
@@ -135,8 +127,116 @@ $(function(){
 				});
 			}
     	}
+
+
+    	$(setTimeout(function(){
+
+    		var chk1s 	= $('#chk-left-1').attr('checked');
+    		var chk2s	= $('#chk-right-1').attr('checked');
+
+    		console.log(chk1s,chk2s);
+
+    		if (chk1s == 'undefined' && chk2s == 'undefined') {
+    			$('.bottom .total-2').css({
+    				display: 'block',
+    			});
+    			$('.bottom .total').removeClass('total-show');
+    		}
+    		else 
+    		{
+    			$('.bottom .total-2').css({
+    				display: 'none',
+    			});
+    			$('.bottom .total').addClass('total-show');
+    		}
+
+
+    		if (chk1s == 'checked' && chk2s == 'checked') {
+    			$('.bottom .total-2').css({
+    				display: 'block',
+    			});
+    			$('.bottom .total').removeClass('total-show');
+    		}
+    		else 
+    		{
+    			$('.bottom .total-2').css({
+    				display: 'none',
+    			});
+    			$('.bottom .total').addClass('total-show');
+    		}
+    	}),500);
+
+    	$(setTimeout(function(){
+			filters = [];
+			$('input.val[type=\'checkbox\']:checked').each(function(element) {
+				filters.push(this.value);
+			});
+			var arrr = filter;
+			var summ = 0;
+			for(var i=0;i<arrr.length;i++){
+			    summ = summ + parseInt(arrr[i]);
+			}
+
+			if (summ == '3100') {
+				var cont = $('input.val[type=\'checkbox\']:checked+label td:nth-child(2) span').text();
+				$('.bottom__big').text(cont);
+
+				var contm = $('input.val[type=\'checkbox\']:checked+label td:nth-child(2) b').text();
+				$('.bottom__small').text(contm);
+			}
+			else {
+				$('.bottom__big').text('ВАШ ЗАКАЗ');
+				$('.bottom__small').text('');
+			}
+		}), 600);
     });
 
+	$('.send').click(function(e){
+		var chek1 = $('.inp').attr('value');
+		var chek2 = $('.inp-2').attr('value');
+		var chek3 = $('.inp-3').attr('value');
 
+		if (chek1 == '') {
+			$('.inp').css({
+				border: '1px solid red',
+				background: '#fbb1b1'
+			});
+		}
+		else {
+			$('.inp').css({
+				border: 'none',
+				background: '#ffffff'
+			});
+		}
+
+		if (chek2 == '') {
+			$('.inp-2').css({
+				border: '1px solid red',
+				background: '#fbb1b1'
+			});
+		}
+		else {
+			$('.inp-2').css({
+				border: 'none',
+				background: '#ffffff'
+			});
+		}
+
+		if (chek3 == '') {
+			$('.inp-3').css({
+				border: '1px solid red',
+				background: '#fbb1b1'
+			});
+			$('.form .captcha img').css('border-color', 'red');
+		}
+		else {
+			$('.inp-3').css({
+				border: 'none',
+				background: '#ffffff'
+			});
+			$('.form .captcha img').css('border-color', '#907b3c');
+		}
+		return false;
+	});
 });
 
